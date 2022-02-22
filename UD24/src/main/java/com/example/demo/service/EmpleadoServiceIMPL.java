bbd12cb48044d92e6ff998284f110ae8d7a35483
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-22 12:34:23 +0100

    EmpleadoServiceIMPL

--- UD24/src/main/java/com/example/demo/service/EmpleadoServiceIMPL.java
+++ UD24/src/main/java/com/example/demo/service/EmpleadoServiceIMPL.java
@@ -1,5 +1,44 @@
 package com.example.demo.service;
+import java.util.List;
 
-public class EmpleadoServiceIMPL {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.Empleado;
+import com.example.demo.dao.IEmpleadoDAO;
+@Service
+public class EmpleadoServiceIMPL implements IEmpleadoService{
+	
+	@Autowired
+	IEmpleadoDAO IEmpleadoDAO;
+	@Override
+	public List<Empleado> listarEmpleados() {
+		return IEmpleadoDAO.findAll();
+	}
+
+	@Override
+	public Empleado guardarEmpleado(Empleado empleado) {
+		return IEmpleadoDAO.save(empleado);
+	}
+
+	@Override
+	public Empleado empleadoXID(Long id) {
+		return IEmpleadoDAO.findById(id).get();
+	}
+
+	@Override
+	public List<Empleado> listarEmpleadoNombre(String nombre) {
+		return IEmpleadoDAO.findByNombre(nombre);
+	}
+
+	@Override
+	public Empleado actualizarEmpleado(Empleado empleado) {
+		return IEmpleadoDAO.save(empelado);
+	}
+
+	@Override
+	public void eliminarEmpleado(Long id) {
+		IEmpleadoDAO.deleteById(id);
+	}
+	
 }
