f9e95d331986621eb5f9ecc239ac79784655fbab
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-22 12:57:03 +0100

    sqlBien

--- UD24/src/main/resources/data.sql
+++ UD24/src/main/resources/data.sql
@@ -8,8 +8,8 @@ create table empleado(
     fecha date
 );
 
-insert into empleado (nombre, apellido,direccion,dni,fecha)values('Jose','Dev',1234.56, NOW());
-insert into empleado (nombre, apellido,direccion,dni,fecha)values('Juan','Sistemas',4321, NOW());
-insert into empleado (nombre, apellido,direccion,dni,fecha)values('Pedro','Dev',3456, NOW());
-insert into empleado (nombre, apellido,direccion,dni,fecha)values('Jordi','Tester',2345, NOW());
-insert into empleado (nombre, apellido,direccion,dni,fecha)values('Jonatan','Dev',2134, NOW());
+insert into empleado (nombre, trabajo,salario,fecha)values('Jose','Dev',1234.56, NOW());
+insert into empleado (nombre, trabajo,salario,fecha)values('Juan','Sistemas',4321, NOW());
+insert into empleado (nombre, trabajo,salario,fecha)values('Pedro','Dev',3456, NOW());
+insert into empleado (nombre, trabajo,salario,fecha)values('Jordi','Tester',2345, NOW());
+insert into empleado (nombre, trabajo,salario,fecha)values('Jonatan','Dev',2134, NOW());
